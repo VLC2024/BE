508b65f600faa97d63f4850b6117f7e89638fe52
yuchaemin2 <kusuri94@gmail.com>
2024-05-02 19:42:45 +0900

    first commit

---
+++ src/main/java/com/vlc/maeummal/MaeummalApplication.java
@@ -0,0 +1,13 @@
+package com.vlc.maeummal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeummalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaeummalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vlc/maeummal/MaeummalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlc.maeummal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaeummalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
