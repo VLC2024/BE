ca912e266d8b4eebddd2de5401d810b3db95f527
yuchaemin2 <kusuri94@gmail.com>
2024-05-10 14:10:24 +0900

    [Refactor] Delete LessonDto field

--- src/main/java/com/vlc/maeummal/domain/lesson/dto/LessonDTO.java
+++ src/main/java/com/vlc/maeummal/domain/lesson/dto/LessonDTO.java
@@ -2,21 +2,16 @@ package com.vlc.maeummal.domain.lesson.dto;
 
 import lombok.*;
 
-import java.util.List;
-
-import java.util.Date;
-
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class LessonDTO {
-    // 상세 페이지에 반환할 내용
     private String title;
     private String content;
     private String created_at;
     private String view;
     private String category;
     private String difficulty;
-    private String field;
+//    private String field;
 }
