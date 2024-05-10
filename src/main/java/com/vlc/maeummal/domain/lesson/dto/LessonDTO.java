ce459f24d749a7f3848d9cafffec12e5d21ed8ef
yuchaemin2 <kusuri94@gmail.com>
2024-05-10 14:05:09 +0900

    [Test] Commit

--- src/main/java/com/vlc/maeummal/domain/lesson/dto/LessonDTO.java
+++ src/main/java/com/vlc/maeummal/domain/lesson/dto/LessonDTO.java
@@ -1,9 +1,8 @@
 package com.vlc.maeummal.domain.lesson.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.List;
 
 import java.util.Date;
 
@@ -12,6 +11,7 @@ import java.util.Date;
 @AllArgsConstructor
 @Data
 public class LessonDTO {
+    // 상세 페이지에 반환할 내용
     private String title;
     private String content;
     private String created_at;
