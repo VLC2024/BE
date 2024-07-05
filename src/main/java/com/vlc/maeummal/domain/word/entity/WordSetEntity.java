8c293d65ab8751f30ad5e9a792e23f8a072156fe
Kang Chaewon <kang960953@gmail.com>
2024-07-05 16:18:12 +0900

    [FEAT, TEST] ìword, word set entity 생ì성 , repository 생성 및 test

---
+++ src/main/java/com/vlc/maeummal/domain/word/entity/WordSetEntity.java
@@ -0,0 +1,37 @@
+package com.vlc.maeummal.domain.word.entity;
+
+import com.vlc.maeummal.global.common.BaseEntity;
+import com.vlc.maeummal.global.enums.Category;
+import com.vlc.maeummal.global.enums.Gender;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="wordSet")
+public class WordSetEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wordSet_id")
+    private Long id;
+    @Column(nullable=true)
+    private String title;
+    @Column(nullable=true)
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @OneToMany(mappedBy = "wordSet")
+    List <WordEntity> wordEntities = new ArrayList<>();
+
+
+}
