92dd445eaa08dbed539265e343cf50b38822acaa
20210699 <20210699@duksung.ac.kr>
2024-07-28 02:09:42 +0900

    [Fix] CORS

--- src/main/java/com/vlc/maeummal/global/config/WebMvcConfig.java
+++ src/main/java/com/vlc/maeummal/global/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000","https://main.dzudx79pr6bs5.amplifyapp.com/")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true)
