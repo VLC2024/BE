be07d3028f7ed2582d331c3b1c2c742bf4a530ba
Kang Chaewon <kang960953@gmail.com>
2024-08-16 19:15:17 +0900

    [FIX] fix merge error

--- src/main/java/com/vlc/maeummal/domain/member/controller/MatchController.java
+++ src/main/java/com/vlc/maeummal/domain/member/controller/MatchController.java
@@ -1,40 +1,40 @@
-package com.vlc.maeummal.domain.member.controller;
-
-import com.vlc.maeummal.domain.member.entity.MemberEntity;
-import com.vlc.maeummal.domain.member.service.MemberService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/teachers")
-public class MatchController {
-    private final MemberService memberService;
-
-    public MatchController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
-    @PostMapping("/{teacherId}/students/{studentPinCode}")
-    public ResponseEntity<MemberEntity> addStudentToTeacher(
-            @PathVariable Long teacherId,
-            @PathVariable Long studentPinCode) {
-        MemberEntity updatedTeacher = memberService.addStudentToTeacher(teacherId, studentPinCode);
-        return ResponseEntity.ok(updatedTeacher);
-    }
-
-    @DeleteMapping("/{teacherId}/students/{studentPinCode}")
-    public ResponseEntity<MemberEntity> removeStudentFromTeacher(
-            @PathVariable Long teacherId,
-            @PathVariable Long studentPinCode) {
-        MemberEntity updatedTeacher = memberService.removeStudentFromTeacher(teacherId, studentPinCode);
-        return ResponseEntity.ok(updatedTeacher);
-    }
-
-    @GetMapping("/{teacherId}/students")
-    public ResponseEntity<List<MemberEntity>> getStudentsOfTeacher(@PathVariable Long teacherId) {
-        List<MemberEntity> students = memberService.getStudentsOfTeacher(teacherId);
-        return ResponseEntity.ok(students);
-    }
-}
+//package com.vlc.maeummal.domain.member.controller;
+//
+//import com.vlc.maeummal.domain.member.entity.MemberEntity;
+//import com.vlc.maeummal.domain.member.service.MemberService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/teachers")
+//public class MatchController {
+//    private final MemberService memberService;
+//
+//    public MatchController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
+//
+//    @PostMapping("/{teacherId}/students/{studentPinCode}")
+//    public ResponseEntity<MemberEntity> addStudentToTeacher(
+//            @PathVariable Long teacherId,
+//            @PathVariable Long studentPinCode) {
+//        MemberEntity updatedTeacher = memberService.addStudentToTeacher(teacherId, studentPinCode);
+//        return ResponseEntity.ok(updatedTeacher);
+//    }
+//
+//    @DeleteMapping("/{teacherId}/students/{studentPinCode}")
+//    public ResponseEntity<MemberEntity> removeStudentFromTeacher(
+//            @PathVariable Long teacherId,
+//            @PathVariable Long studentPinCode) {
+//        MemberEntity updatedTeacher = memberService.removeStudentFromTeacher(teacherId, studentPinCode);
+//        return ResponseEntity.ok(updatedTeacher);
+//    }
+//
+//    @GetMapping("/{teacherId}/students")
+//    public ResponseEntity<List<MemberEntity>> getStudentsOfTeacher(@PathVariable Long teacherId) {
+//        List<MemberEntity> students = memberService.getStudentsOfTeacher(teacherId);
+//        return ResponseEntity.ok(students);
+//    }
+//}
