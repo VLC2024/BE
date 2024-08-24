4a81cd4732f0ad577b9a8870a18667c190f55e6f
20210699 <20210699@duksung.ac.kr>
2024-08-25 00:32:03 +0900

    Feat: 마이페이지 조회

--- src/main/java/com/vlc/maeummal/domain/member/controller/MyPageController.java
+++ src/main/java/com/vlc/maeummal/domain/member/controller/MyPageController.java
@@ -1,11 +1,42 @@
 package com.vlc.maeummal.domain.member.controller;
 
+import com.vlc.maeummal.domain.member.dto.StudentDTO;
+import com.vlc.maeummal.domain.member.dto.TeacherDTO;
+import com.vlc.maeummal.domain.member.entity.MemberEntity;
+import com.vlc.maeummal.domain.member.repository.MemberReposirotyUsingId;
+import com.vlc.maeummal.domain.member.service.MemberService;
+import com.vlc.maeummal.domain.member.service.MyPageService;
+import com.vlc.maeummal.global.apiPayload.ApiResponse;
+import com.vlc.maeummal.global.converter.UserAuthorizationConverter;
+import com.vlc.maeummal.global.enums.Role;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/auth")
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class MyPageController {
+    private final UserAuthorizationConverter userAuthorizationConverter;
+    private final MemberReposirotyUsingId memberReposirotyUsingId;
+    private final MyPageService myPageService;
+    @GetMapping
+    public ResponseEntity<?> getMemberInfo(){
+        Long memberId = userAuthorizationConverter.getCurrentUserId();
+        MemberEntity member = memberReposirotyUsingId.findById(memberId).orElseThrow(() -> new RuntimeException(""));
+        if (member.getRole().equals(Role.STUDENT)){
+            StudentDTO.GetStudentInfo result =  myPageService.getStudentInfo(member);
+            return ResponseEntity.ok(ApiResponse.onSuccess(result));
+        } else if(member.getRole().equals(Role.TEACHER)){
+            TeacherDTO.GetTeacherInfo result = myPageService.getTeacherInfo(member);
+            return ResponseEntity.ok(ApiResponse.onSuccess(result));
+        } else {
+            return ResponseEntity.badRequest().body("잘못된 접근입니다. 회원가입과 로그인을 진행해주세요");
+        }
+    }
+
 }
--- src/main/java/com/vlc/maeummal/domain/member/service/MyPageService.java
+++ src/main/java/com/vlc/maeummal/domain/member/service/MyPageService.java
@@ -1,9 +1,30 @@
 package com.vlc.maeummal.domain.member.service;
 
+import com.vlc.maeummal.domain.member.dto.StudentDTO;
+import com.vlc.maeummal.domain.member.dto.TeacherDTO;
+import com.vlc.maeummal.domain.member.entity.MemberEntity;
+import com.vlc.maeummal.domain.member.repository.MemberReposirotyUsingId;
+import com.vlc.maeummal.domain.member.repository.MemberRepository;
+import com.vlc.maeummal.global.converter.UserAuthorizationConverter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class MyPageService {
+    private final MemberRepository memberRepository;
+    private final MemberReposirotyUsingId memberReposirotyUsingId;
+    private final UserAuthorizationConverter userAuthorizationConverter;
+
+    public StudentDTO.GetStudentInfo getStudentInfo(MemberEntity member){
+        return StudentDTO.GetStudentInfo.getStudentInfo(member);
+    }
+
+    public TeacherDTO.GetTeacherInfo getTeacherInfo(MemberEntity member){
+        return TeacherDTO.GetTeacherInfo.getTeacherInfo(member);
+    }
 }
