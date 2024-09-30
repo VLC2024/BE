5a2592896d09eab3fa7c3d2572f41a3dc766124c
20210699 <20210699@duksung.ac.kr>
2024-09-30 13:30:04 +0900

    [Feat] 전체 템플릿 조회 시 temp4 전체 조회 기능 추가

--- src/main/java/com/vlc/maeummal/domain/template/common/service/TemplateService.java
+++ src/main/java/com/vlc/maeummal/domain/template/common/service/TemplateService.java
@@ -5,6 +5,7 @@ import com.vlc.maeummal.domain.template.common.dto.TemplateResponseDTO;
 import com.vlc.maeummal.domain.template.template1.repository.Template1Repository;
 import com.vlc.maeummal.domain.template.template2.repository.Template2Repository;
 import com.vlc.maeummal.domain.template.template3.repository.Template3Repository;
+import com.vlc.maeummal.domain.template.template4.repository.Template4Repository;
 import com.vlc.maeummal.domain.template.template5.repository.Template5Repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class TemplateService {
     private final Template1Repository template1Repository;
     private final Template2Repository template2Repository;
     private final Template3Repository template3Repository;
-//    private final Template4Repository template4Repository;
+    private final Template4Repository template4Repository;
     private final Template5Repository template5Repository;
 
 
@@ -31,6 +32,7 @@ public class TemplateService {
         templateEntityList.addAll(template1Repository.findAll());
         templateEntityList.addAll(template2Repository.findAll());
         templateEntityList.addAll(template3Repository.findAll());
+        templateEntityList.addAll(template4Repository.findAll());
         templateEntityList.addAll(template5Repository.findAll());
 
         // 생성일 기준으로 오름차순 정렬
